377dd5f2a00864ce1f303cb3a8220a81ef95cbb1
= <=>
2021-03-14 12:43:16 +0530

    Day 4 completed

--- lib/Pages/loginPage.dart
+++ lib/Pages/loginPage.dart
@@ -1,21 +1,77 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.light(),
       home: SafeArea(
         child: Scaffold(
-          body: Material(
-            child: Center(
-              child: Text(
-                'Login page',
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.brown,
-                  fontWeight: FontWeight.bold,
-                ),
+          body: SingleChildScrollView(
+            child: Material(
+              color: Colors.white,
+              child: Column(
+                children: <Widget>[
+                  Image.asset(
+                    'assets/images/Login_Image.png',
+                    fit: BoxFit.cover,
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Text(
+                    'Welcome To My App',
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'SourceSansPro',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 6, vertical: 20),
+                    child: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          decoration: InputDecoration(
+                            hintText: "Enter Your Name",
+                            labelText: "UserName",
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 0, vertical: 20),
+                          child: TextFormField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              hintText: "Enter Password",
+                              labelText: "Password",
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            print('Printed');
+                          },
+                          child: Text('Login'),
+                          style: TextButton.styleFrom(),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
@@ -24,3 +80,7 @@ class LoginPage extends StatelessWidget {
     );
   }
 }
+/**
+ * singleChlidScrollView help to fix overflow due to keyboard
+ * 
+ */
--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,24 @@ import 'Pages/homePage.dart';
 import 'Pages/loginPage.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    bringop(paisa: 700, plactic: true); //Function call
     return MaterialApp(
       // home: HomePage(),
-      //*This is for dark
-      //theme: ThemeData.dark(), //This is for Dark theme
-      // themeMode: ThemeMode.dark,
-      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
-      initialRoute: "/home",
       routes: {
         "/": (context) => LoginPage(),
         "/home": (context) => HomePage(),
@@ -27,9 +28,13 @@ class MyApp extends StatelessWidget {
       },
     );
   }
-
-  bringop({paisa = 100, @required bool plactic}) {
-    //default parameter if we write @requied then we must write that argument
-    print('paisa is $paisa and $plactic');
-  }
 }
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+    // bringop(paisa: 700, plactic: true); //Function call
+  // bringop({paisa = 100, @required bool plactic}) {
+     //default parameter if we write @requied then we must write that argument
+  //   print('paisa is $paisa and $plactic');
+  // }
+  //theme: ThemeData.dark(), //This is for Dark theme
+  // themeMode: ThemeMode.dark,
